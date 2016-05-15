7086702976a9dae4229b8716e5b94e2334d94dcb
Eric Redmond <eric.redmond@nike.com>
2016-05-14 19:50:23 -0700

    remove sampels dir

--- samples/EFSBackup/efs-backup-end.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Input arguments
-interval=$1
-efsid=$2
-
-echo "sudo touch /mnt/backups/$efsid/$interval.0/"
-sudo touch /mnt/backups/$efsid/$interval.0/
-echo "$interval: completed successfully"
